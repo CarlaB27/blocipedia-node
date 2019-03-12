005df879bcfc442b638f2bee6a33c0100293b45a
Carla <carla.f.bates@hotmail.com>
2019-03-11 23:18:54 -0400

    checkpoint-2

---
+++ spec/integration/static_spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/";
+
+describe("routes : static", () => {
+    describe("GET /", () => {
+        it("should return status code 200", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+});
---
+++ src/routes/static.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// eslint-disable-next-line no-unused-vars
+router.get("/", (req, res, next) => {
+    res.send("Blocipedia");
+});
+
+module.exports = router;
